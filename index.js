59d2b4e51e632ef996017827163e1fec01b5df83
kavya <kavyashrees009@gmail.com>
2026-02-24 10:53:39 +0530

    feat(index): Add additional console log statement for platform description

--- index.js
+++ index.js
@@ -2,3 +2,6 @@
     console.log("Hello, World");
     console.log("Welcome to Aivagam");
     console.log("This is a sample code snippet for Aivagam");
+    console.log("Aivagam is a platform for learning and sharing knowledge");
+
+    
